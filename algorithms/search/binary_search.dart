2b4365263ff9d97cddd90a5ca3111fbacf5c144e
LEADER <godstark82@gmail.com>
2024-08-14 08:26:00 +0530

    added iterative and recursive binary search

--- algorithms/search/binary_search.dart
+++ algorithms/search/binary_search.dart
@@ -1,32 +1,51 @@
-
 final List<int> nums = [1, 4, 8, 6, 45, 87, 23, 898, 3, 567];
 
 void main(List<String> args) {
   nums.sort();
-  Stopwatch stopwatch = Stopwatch();
 
-  stopwatch.start();
-  int index = binarySearch(nums, 87, 0, nums.length - 1);
+  int index = 0;
+
   if (index != -1) {
     print('Value found at index $index');
   } else {
     print('Value not found');
   }
-  print(stopwatch.elapsed.inMilliseconds);
 }
 
-int binarySearch(List<int> nums, int value, int low, int high) {
-  if (low <= high) {
-    int mid = (low + high) ~/ 2;
+int iterativeBinarySearch(List<int> nums, value) {
+  int left = 0;
+  int right = nums.length - 1;
+
+  while (left <= right) {
+    int mid = left + (right - left) ~/ 2;
+
+    if (mid == nums[mid]) {
+      return mid;
+    }
+
+    if (value < nums[mid]) {
+      right = mid - 1;
+    }
+    if (value > nums[mid]) {
+      left = mid + 1;
+    }
+  }
+  return -1;
+}
+
+int recursiveBinarySearch(List<int> nums, int value, int left, int right) {
+  if (left <= right) {
+    int mid = left + (right - left) ~/ 2;
 
     if (value == nums[mid]) {
       return mid;
     }
 
+    if (value < nums[mid]) {
+      return recursiveBinarySearch(nums, value, left, mid - 1);
+    }
     if (value > nums[mid]) {
-      return binarySearch(nums, value, mid + 1, high);
-    } else {
-      return binarySearch(nums, value, low, mid - 1);
+      return recursiveBinarySearch(nums, value, mid + 1, right);
     }
   }
 
